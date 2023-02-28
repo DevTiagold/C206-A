8ce2e7e3d795cff2af083c20a3e774007ff636ed
DevTiagold <tiagoaugustocostacarvalho@gmail.com>
2023-02-28 14:48:03 -0300

    AulaPoo

---
+++ Exercicios_C206/src/MainZ.java
@@ -0,0 +1,16 @@
+public class MainZ{
+    public static void main(String[] args) {
+        Zumbi c = new Zumbi();
+        c.nome = "Chris";
+        if(c.alimentado == true){
+            System.out.println("O zumbi " + c.nome + " está alimentado");
+        }
+        if(c.vida > 0){
+            System.out.println("O zumbi " + c.nome + " está vivo");
+        }
+
+        Zumbi tiago = new Zumbi();
+        tiago.vida = 30;
+
+    }
+}
---
+++ Exercicios_C206/src/Zumbi.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Zumbi {
+
+    //Membros da classe zumbi
+    String nome;
+    double vida = 50;
+    boolean alimentado = true;
+
+    public void perdervida(){
+        vida -= 20;
+    }
+}
+
+
